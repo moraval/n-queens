d454fe92a7b52faf15806691bc3655c048c7e0d1
thejsj <jorge.silva@thejsj.com>
2014-11-06 19:08:06 -0800

    Starting distributions

--- src/worker/client.js
+++ src/worker/client.js
@@ -1,14 +1,58 @@
 $(document).ready(function () {
-    var worker = new Worker('dist/worker/worker.js');
 
-    worker.addEventListener('message', function (e) {
-        console.log(e.data);
-        var str = '<div class="message">';
-        str += 'Solutions for n == ' + e.data.n + ': ' + e.data.solution;
-        str += '</div>';
-        $('.messages').append(str);
-        worker.postMessage(e.data.n + 1);
-    }, false);
-    worker.postMessage(4); // Send data to our worker.
+    var NQueensDistributionHandler = function (n) {
 
+        var self = {},
+            __self = {};
+
+        __self.n = n;
+
+        __self.init = function () {
+            __self.worker = new Worker('dist/worker/worker.js');
+            __self.$messages = $('.messages');
+        };
+
+        __self.startQueue = function (n) {
+            if (n === 0 || n === 1) return {
+                n: n,
+                solution: 1
+            };
+            if (n === 2 || n === 3) return {
+                n: n,
+                solution: 0
+            };
+            var board = new Board({
+                n: n
+            });
+            var solutionCount = 0;
+            var queue = new NQueensJobQueueHandler(
+                [new NQueensJob(new Board(board.rows()), 0, n)],
+                0
+            );
+            while (queue.getJobCount() > 0) {
+                queue.execute();
+            }
+            return {
+                n: n,
+                solution: queue.solutionCount
+            };
+            __self.worker.addEventListener('message', function (e) {
+                console.log(e.data);
+                __self.pushMessage(e.data.n, e.data.solution);
+                worker.postMessage(e.data.n + 1);
+            }, false);
+            __self.worker.postMessage({
+                n: n
+            }); // Send data to our worker.
+        };
+        __self.pushMessage = function (n, solution) {
+            var str = '<div class="message">';
+            str += 'Solutions for n == ' + e.data.n + ': ' + e.data.solution;
+            str += '</div>';
+            __self.$messages.append(str);
+        };
+        __self.init();
+        return self;
+    };
+    var distributionHandler = new NQueensDistributionHandler();
 });
--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,15 +1,11 @@
 // This file is a Backbone Model (don't worry about what that means)
 // It's part of the Board Visualizer
 // The only portions you need to work on are the helper functions (below)
-
-var __self = {};
-__self.Board = require('./Board');
-__self.countNQueensSolutions = require('./countNQueensDistributed');
-
+var NQueensJobQueueHandler = require('./NQueensJobQueueHandler');
 try {
     if (self !== undefined) {
         self.addEventListener('message', function (e) {
-            self.postMessage(__self.countNQueensSolutions(e.data));
+            self.postMessage(new NQueensJobQueueHandler(e.data.n));
         }, false);
     }
 } catch (err) {}
